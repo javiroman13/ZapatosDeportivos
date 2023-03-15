79c07aef4455dc3527b8e57ed3c277c44e11743d
javier roman <22670018@itiguala.edu.mx>
2023-03-15 12:54:43 -0600

    se crea proyecto zapatos deportivos

---
+++ src/TheSneakers.java
@@ -0,0 +1,15 @@
+public class TheSneakers {
+    public static void main(String[] args) {
+        Sneakers zapatos = new Sneakers();
+        zapatos.setModel(2389899);
+        zapatos.setSize(7.5);
+        zapatos.setColor("blancos");
+        zapatos.setMarca("nike");
+
+
+
+       zapatos.toString();
+    }
+
+
+}
